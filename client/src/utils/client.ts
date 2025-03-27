db1c0b699187b51dcc1c3ecfffff94d5d9c58f1a
Daniel Tsagli <Danieltsagli960@gmail.com>
2025-03-27 09:08:47 +0000

    Update client.ts

--- client/src/utils/client.ts
+++ client/src/utils/client.ts
@@ -2,7 +2,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: process.env.SERVER_GRAPHQL_BASE_URL,
+  uri: import.meta.env.VITE_API_URL,
   cache: new InMemoryCache(),
 });
 
