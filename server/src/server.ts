5cc2cb613ab5b74c954e4771bdf5cf2fad987626
Daniel Tsagli <Danieltsagli960@gmail.com>
2025-03-27 08:34:11 +0000

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -51,13 +51,10 @@ app.use(express.json());
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+app.use(express.static('../client/dist'));
 
 app.get('*', (_req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile('../client/dist/index.html');
 });
 
 db.once('open', async () => {
